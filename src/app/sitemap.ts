569877f415d36caa533cf41fae2fbd0acc9db3cb
Tyler Kirwan <tkirwan123@gmail.com>
2025-07-04 16:41:30 +0000

    fix: Correct sitemap URL

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,7 @@
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  // Using a placeholder domain. You should replace this with your actual domain.
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://numawise.com';
+  const siteUrl = 'https://www.askravia.com';
   return [
     {
       url: siteUrl,
